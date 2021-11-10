e45ba773448150d729399c4f60595cf6bf704f37
Hyojeong Jang <hyojeong@Hyojeongui-MacBookPro-2.local>
2021-11-09 23:08:32 -0800

    config: set typeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: '',
+      password: '',
+      database: '',
+      entities: [], // 사용할 entity의 클래스명
+      synchronize: true, // false로 해두는 게 안전
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
